73b7402420d5d49268cc3d69c38f076e72d5ccbb
TrinhDucLoc <westudymore@gmail.com>
2023-10-01 10:43:22 +0700

    Q4 done

---
+++ DSA_Training_25Q/src/Q4_reverse.java
@@ -0,0 +1,44 @@
+public class Q4_reverse {
+    public static void main(String[] args) {
+        System.out.println(reverse(1234));
+        System.out.println(reverse(12005));
+        System.out.println(reverse(1));
+        System.out.println(reverse(-1233));
+        System.out.println(reverse(0));
+    }
+
+    public static int reverse(int n){
+        int flag = 1;
+        int reverse = 0;
+        if(n == 0) {
+            return 0;
+        }
+        if(n < 0) {
+            flag = -1;
+            n = -n;
+        }
+        while(n != 0) {
+//            last digit
+            int lastDigit = n % 10;
+//            reverse add last digit
+            reverse = reverse * 10 + lastDigit;
+//            num remove last digit
+            n /= 10;
+        }
+
+        return flag*reverse;
+    }
+}
+
+
+// Solution
+// int reverse(int n)
+// if(n = 0) => return 0;
+// if(n < 0)
+// => flag = -1
+// => n = -n
+// if(n > 0)
+// while(n != 0)
+// int digit = n % 10;
+// reverse = n*10 + digit;
+// n = n/10;
