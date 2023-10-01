ba5290918c4cf4b40e0ffdb9d25e944d56f8d75f
TrinhDucLoc <westudymore@gmail.com>
2023-10-01 10:20:47 +0700

    Q3 done

---
+++ DSA_Training_25Q/src/Q3_newArrayWithStartLengthPosition.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Q3_newArrayWithStartLengthPosition {
+    public static void main(String[] args) {
+        System.out.println(newArrayWithStartLengthPosition(new char[] {'a','b','c'}, 0, 4));
+        System.out.println(newArrayWithStartLengthPosition(new char[]{'a','b','c'}, 0,3));
+        System.out.println(newArrayWithStartLengthPosition(new char[]{'a','b','c'}, 0, 2));
+        System.out.println(newArrayWithStartLengthPosition(new char[]{'a','b','c'}, 1, 3));
+        System.out.println(newArrayWithStartLengthPosition(new char[]{'a','b','c'}, 1, 2));
+        System.out.println(newArrayWithStartLengthPosition(new char[]{'a','b','c'}, -1, 2));
+        System.out.println(newArrayWithStartLengthPosition(new char[]{}, 0, 1));
+    }
+
+    public static String newArrayWithStartLengthPosition(char[] a, int start, int len) {
+//        validate return null
+        if(start < 0 || len < 0 || start + len > a.length) {
+            return null;
+        }
+        char[] newArray = new char[len];
+        int j = 0;
+        for(int i=start; i<(start + len); i++) {
+            newArray[j] += a[i];
+            j++;
+        }
+        return Arrays.toString(newArray);
+    }
+}
+
+
+//Solution
+// if(startPosition + lengthPosition > lengthArray) => return null
+// if(startPosition < 0 || lengthPosition < 0) => return null
