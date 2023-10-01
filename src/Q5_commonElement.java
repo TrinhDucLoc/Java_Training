78ba75e6a4d90b2347c88bbbc48afa28e23697de
TrinhDucLoc <westudymore@gmail.com>
2023-10-01 11:33:44 +0700

    Refactor folder

---
+++ src/Q5_commonElement.java
@@ -0,0 +1,14 @@
+public class Q5_commonElement {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+// Solution
+// validate
+// if(firstArray == null || secondArray == null) => return null
+// if(firstArray.length == 0 || secondArray.length == 0) => return null
+//
