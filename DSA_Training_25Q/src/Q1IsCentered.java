f90bfaa3c660671483b270b0cb3dfc2742bc2b04
TrinhDucLoc <westudymore@gmail.com>
2023-10-01 09:32:53 +0700

    Training Java 001

---
+++ DSA_Training_25Q/src/Q1IsCentered.java
@@ -0,0 +1,29 @@
+public class Q1IsCentered {
+    public static void main(String[] args) {
+        System.out.println(isCentered(new int[]{1,2,3,4,5}));
+        System.out.println(isCentered(new int[]{3,2,1,4,5}));
+        System.out.println(isCentered(new int[]{3,2,1,4,1}));
+        System.out.println(isCentered(new int[]{1,2,3,4}));
+        System.out.println(isCentered(new int[]{}));
+        System.out.println(isCentered(new int[]{10}));
+    }
+
+    public static int isCentered(int[] items) {
+        if(items.length == 0 || items.length % 2 == 0) {
+            return 0;
+        }
+        int indexMiddle = items.length / 2;
+        for(int i=0; i<items.length; i++) {
+            if(items[indexMiddle] >= items[i] && i != indexMiddle) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+
+
+//Note solution:
+// Check array have odd element or length array == 0 => return 0
+// IF center element is min => this is isCentered => return 1
