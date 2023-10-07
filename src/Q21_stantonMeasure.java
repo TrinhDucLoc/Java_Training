eff8cf15f1af4dcc2c37ed681e30268575f2575d
TrinhDucLoc <westudymore@gmail.com>
2023-10-08 05:48:34 +0700

    Q21 done

---
+++ src/Q21_stantonMeasure.java
@@ -0,0 +1,28 @@
+public class Q21_stantonMeasure {
+    public static void main(String[] args) {
+        System.out.println(stantonMeasure(new int[]{1}));
+        System.out.println(stantonMeasure(new int[]{0}));
+        System.out.println(stantonMeasure(new int[]{3,1,1,4}));
+        System.out.println(stantonMeasure(new int[]{1,3,1,1,3,3,2,3,3,3,4}));
+        System.out.println(stantonMeasure(new int[]{}));
+    }
+
+    static int stantonMeasure(int[] a) {
+        int countOne = 0;
+        int countNum = 0;
+
+        for(int i=0; i<a.length; i++) {
+            if(a[i] == 1) {
+                countOne++;
+            }
+        }
+
+        for(int i=0; i<a.length; i++) {
+            if(a[i] == countOne) {
+                countNum++;
+            }
+        }
+
+        return countNum;
+    }
+}
