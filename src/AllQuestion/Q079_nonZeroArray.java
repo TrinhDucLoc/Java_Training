754ccec4dc646fd2aff13b3a69b8fddd889ba764
TrinhDucLoc <westudymore@gmail.com>
2023-10-24 17:51:37 +0700

    Q079 nonZeroArray done

--- src/AllQuestion/Q079_nonZeroArray.java
+++ src/AllQuestion/Q079_nonZeroArray.java
@@ -11,7 +11,7 @@ public class Q079_nonZeroArray {
 
     static int nonZeroArray(int[] a) {
         for(int i=0; i<a.length; i++) {
-            if(a[i] == 0) {it ad
+            if(a[i] == 0) {
                 return 0;
             }
         }
