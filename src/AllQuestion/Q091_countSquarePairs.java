1cc2b851fcd979d9d1076f9b415316d2a7aeb5f0
TrinhDucLoc <westudymore@gmail.com>
2023-10-26 06:17:40 +0700

    Q091 countSquarePairs done

---
+++ src/AllQuestion/Q091_countSquarePairs.java
@@ -0,0 +1,40 @@
+package AllQuestion;
+
+public class Q091_countSquarePairs {
+    public static void main(String[] args) {
+        System.out.println(countSquarePairs(new int[]{9, 0, 2, -5, 7} ));
+        System.out.println(countSquarePairs(new int[]{9}));
+    }
+
+    static int countSquarePairs(int[] a) {
+//        validate
+        if(a.length < 2) {
+            return 0;
+        }
+//        solutionit s
+        int count = 0;
+        for(int i=0; i<a.length-1; i++) {
+            if(a[i] <= 0) {
+                continue;
+            }
+            for(int j=i+1; j<a.length; j++) {
+                if(a[j] <= 0) {
+                    continue;
+                }
+                if(isPerfectSquare(a[i] + a[j]) == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    static int isPerfectSquare(int n) {
+        for(int i=0; i<=n; i++) {
+            if(i*i == n) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
