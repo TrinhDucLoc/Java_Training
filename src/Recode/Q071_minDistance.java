9f9a2cc9f441ff5012135a6acd55ac6935c101bf
TrinhDucLoc <westudymore@gmail.com>
2023-10-30 20:13:17 +0700

    Q071 minDistance recode

---
+++ src/Recode/Q071_minDistance.java
@@ -0,0 +1,24 @@
+package Recode;
+
+public class Q071_minDistance {
+    public static void main(String[] args) {
+        System.out.println(minDistance(13013));
+        System.out.println(minDistance(8));
+    }
+
+    static int minDistance(int n) {
+        int distance = Integer.MAX_VALUE;
+        int previousFactor = 1;
+        int currentFactor = 0;
+        for(int i=2; i<=n; i++) {
+            if(n%i == 0) {
+                currentFactor = i;
+                if(currentFactor - previousFactor < distance) {
+                    distance = currentFactor - previousFactor;
+                }
+                previousFactor = currentFactor;
+            }
+        }
+        return distance;
+    }
+}
