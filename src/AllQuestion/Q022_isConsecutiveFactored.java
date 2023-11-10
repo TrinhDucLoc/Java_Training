b5a646ea3a5702851573ddc991276763763721c5
TrinhDucLoc <westudymore@gmail.com>
2023-11-10 14:12:43 +0700

    Q022 isConsecutiveFactored done

---
+++ src/AllQuestion/Q022_isConsecutiveFactored.java
@@ -0,0 +1,28 @@
+package AllQuestion;
+
+public class Q022_isConsecutiveFactored {
+    public static void main(String[] args) {
+        System.out.println(isConsecutiveFactored(24));
+        System.out.println(isConsecutiveFactored(105));
+        System.out.println(isConsecutiveFactored(90));
+        System.out.println(isConsecutiveFactored(23));
+        System.out.println(isConsecutiveFactored(15));
+        System.out.println(isConsecutiveFactored(2));
+        System.out.println(isConsecutiveFactored(0));
+        System.out.println(isConsecutiveFactored(-12));
+    }
+
+    static int isConsecutiveFactored(int n) {
+        for(int i=2; i<n; i++) {
+            if(n % i == 0 && n % (i+1) == 0) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+
+// Requirement:
+// A positive number n is consecutive-factored
+// if and only if it has factors
+// i and j where i > 1, j > 1 and j = i + 1
