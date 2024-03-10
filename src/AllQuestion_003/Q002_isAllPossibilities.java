c49bad237b39a68a32d7b4f1bb79e8a1ae84a3f6
TrinhDucLoc <westudymore@gmail.com>
2024-03-10 16:35:49 +0700

    Q002 done

---
+++ src/AllQuestion_003/Q002_isAllPossibilities.java
@@ -0,0 +1,38 @@
+package AllQuestion_003;
+
+public class Q002_isAllPossibilities {
+    public static void main(String[] args) {
+        System.out.println(isAllPossibilities(new int[]{1, 2, 0, 3}));
+        System.out.println(isAllPossibilities(new int[]{3, 2, 1, 0}));
+        System.out.println(isAllPossibilities(new int[]{1, 2, 4, 3}));
+        System.out.println(isAllPossibilities(new int[]{0, 2, 3}));
+        System.out.println(isAllPossibilities(new int[]{0}));
+        System.out.println(isAllPossibilities(new int[]{}));
+    }
+
+    public static int isAllPossibilities(int[] a) {
+        // validation
+        if(a.length == 0) {
+            return 0;
+        }
+
+        // solution
+        for(int i=0; i<a.length-1; i++) {
+            for(int j=i+1; j<a.length; j++) {
+                if(a[i] > a[j]) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+        for(int i=0; i<=a.length-1; i++) {
+            if(i != a[i]) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+}
